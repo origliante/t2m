65a4e7c17df48af08a88d80a2b89184c67eb2718
origliante <6919114+origliante@users.noreply.github.com>
2017-08-28 12:41:28 +0200

    centos.sh add to cleanup yum cache

--- provisioning/centos.sh
+++ provisioning/centos.sh
@@ -3,9 +3,11 @@
 yum -y install psmisc
 yum -y install python
 yum -y update python
+yum clean all
 
 # fix for the private network interface coming up with a different
 # address until network restart 
 systemctl stop network
 systemctl start network
+exit 0
 
