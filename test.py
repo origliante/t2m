852b34756ce4e7085a17b57bc106ce0463ea9847
origliante <6919114+origliante@users.noreply.github.com>
2017-08-28 12:41:57 +0200

    test.py parametrizable via method attributes

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import socket
 import os
 import time
 
+
+
 def client(ip, port, message):
     sock = socket.socket(socket.AF_INET,
                      socket.SOCK_DGRAM)
@@ -12,28 +14,35 @@ def client(ip, port, message):
 
 class VagrantTestCase(unittest.TestCase):
     """Vagrant tests for `t2m.py`."""
+
+    T2M_IP = '192.168.42.42'
+    T2M_PORT = 1999
+
     def setUp(self):
         os.system("rm -f .t2m.log")
         os.system("vagrant provision")
-        pass
+
+    def get_logdata(self):
+        logdata = file('.t2m.log', 'r').read()
+        return logdata
 
     def test_invalid_msg(self):
-        client('192.168.42.42', 1999, 'test')
+        client(self.T2M_IP, self.T2M_PORT, 'test')
         #FIXME: how to force a log flush?
         time.sleep(2)
-        logdata = file('.t2m.log', 'r').read()
+        logdata = self.get_logdata()
         #print(logdata)
         self.assertTrue("server received 'test' from" in logdata)
         self.assertTrue("invalid message received." in logdata)
 
     def test_proper_msg(self):
-        client('192.168.42.42', 1999, '[3/6/2007 12:12] Hello World!')
-        logdata = file('.t2m.log', 'r').read()
+        client(self.T2M_IP, self.T2M_PORT, '[3/6/2007 12:12] Hello World!')
+        logdata = self.get_logdata()
         #print(logdata)
         self.assertTrue("" in logdata)
 
 
-
 if __name__ == '__main__':
     unittest.main()
+
 
